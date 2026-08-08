07e193c2ab7901cfaa2f892d836bf2d70d53659d
Saddock Kabandana <aimegetz@gmail.com>
2026-08-08 12:19:38 +0300

    deploy

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,5 @@
 // ---------------------------------------------------------------------------
 // lib/api.ts - legacy compatibility shim
-//
 // It re-exports everything from lib/api/index.ts
 // ---------------------------------------------------------------------------
 export * from './api/index';
